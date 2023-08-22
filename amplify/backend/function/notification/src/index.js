a95506092f73d1647d7ebd4a04cc0c46023597e2
Aksheg <akindeleoluwasegun01@gmail.com>
2023-08-22 10:41:55 +0100

    sms notification lambda function

--- amplify/backend/function/notification/src/index.js
+++ amplify/backend/function/notification/src/index.js
@@ -1,17 +1,51 @@
+const AWS = require("aws-sdk");
 
+const sqsQueueUrl = process.env.AWS_QUEUE_URL;
+const snsTopicArn = process.env.AWS_TOPIC_ARN;
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+const sqs = new AWS.SQS({ region: process.env.REGION });
+const sns = new AWS.SNS({ region: process.env.REGION });
+
+exports.handler = async (event, context) => {
+  try {
+    // Poll message from SQS
+    const receiveParams = {
+      QueueUrl: sqsQueueUrl,
+      MaxNumberOfMessages: 1,
+      WaitTimeSeconds: 20,
     };
+    const sqsResponse = await sqs.receiveMessage(receiveParams).promise();
+
+    if (!sqsResponse.Messages || sqsResponse.Messages.length === 0) {
+      console.log("No messages found in SQS queue.");
+      return;
+    }
+
+    const message = sqsResponse.Messages[0];
+    const messageBody = JSON.parse(message.Body);
+
+    const PhoneNumber = messageBody.phoneNumber;
+
+    // Generate SMS template
+    const smsTemplate = `Message for ${PhoneNumber} from SQS: ${messageBody.message}`;
+
+    // Publish message to SNS
+    const snsParams = {
+      Message: smsTemplate,
+      TopicArn: snsTopicArn,
+    };
+    await sns.publish(snsParams).promise();
+
+    // Delete processed message from SQS
+    const deleteParams = {
+      QueueUrl: sqsQueueUrl,
+      ReceiptHandle: message.ReceiptHandle,
+    };
+    await sqs.deleteMessage(deleteParams).promise();
+
+    console.log("Message processed and sent via SNS.");
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
 };
