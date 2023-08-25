8175f6c2fb009f2e1d286d29130eaa3c0a73e6de
OlawaleP <99323873+OlawaleP@users.noreply.github.com>
2023-08-25 19:50:13 +0100

    modified lambda function and updated test cases

--- amplify/backend/function/notification/src/__test__/index.test.js
+++ amplify/backend/function/notification/src/__test__/index.test.js
@@ -1,5 +1,8 @@
 const AWS = require("aws-sdk");
 const { handler } = require("../index");
+const dotenv = require("dotenv");
+dotenv.config()
+const PHONE_NUMBER =process.env.PHONE_NUMBER;
 
 jest.mock("aws-sdk");
 
@@ -33,7 +36,7 @@ describe("Lambda Function", () => {
       promise: jest.fn().mockResolvedValueOnce({}),
     });
 
-    await handler();
+    await handler(PHONE_NUMBER );
 
     expect(AWS.SQS.prototype.receiveMessage).toHaveBeenCalledWith({
       QueueUrl: process.env.AWS_QUEUE_URL,
@@ -56,9 +59,11 @@ describe("Lambda Function", () => {
       promise: jest.fn().mockResolvedValueOnce(mockMessage),
     });
 
-    await handler();
+  const result =  await handler(PHONE_NUMBER);
 
     expect(AWS.SNS.prototype.publish).not.toHaveBeenCalled();
+    expect(result.statusCode).toBe(404);
+    expect(result.body).toBe("No messages found in SQS queue.");
   });
 
   it("should handle errors", async () => {
@@ -66,14 +71,21 @@ describe("Lambda Function", () => {
         AWS.SQS.prototype.receiveMessage = jest.fn().mockReturnValueOnce({
           promise: jest.fn().mockRejectedValueOnce(mockError),
         });
-    
         try {
-          await handler();
+          await handler(PHONE_NUMBER);
         } catch (error) {
           expect(error.statusCode).toBe(500);
           if (error.message) {
             expect(error.message).toBe("Mocked error");
           }
         }
+      });
+
+      it("should handle no phone number provided error", async () => {
+
+       const result = await handler("");
+       expect(result.statusCode).toBe(404);
+       expect(result.body).toBe("No phone number provided.");
+     
       });
 });
--- amplify/backend/function/notification/src/index.js
+++ amplify/backend/function/notification/src/index.js
@@ -4,12 +4,9 @@ dotenv.config()
 
 const {
   AWS_QUEUE_URL,
-  AWS_TOPIC_ARN,
   REGION,
   PHONE_NUMBER,
 } = process.env;
-
-
 
 const createAWSInstanceWithRegion = (Service, region) => {
   return new Service({ region });
@@ -18,14 +15,13 @@ const createAWSInstanceWithRegion = (Service, region) => {
 const sqs = createAWSInstanceWithRegion(AWS.SQS, REGION);
 const sns = createAWSInstanceWithRegion(AWS.SNS, REGION);
 
-exports.handler = async () => {
+exports.handler = async (phoneNumber) => {
 
   try {
-  
-    if (!PHONE_NUMBER) {
-      console.log("No phone number provided.");
+
+    if (!phoneNumber) {
       return {
-        statusCode: 200,
+        statusCode: 404,
         body: "No phone number provided.",
       };
     }
@@ -39,9 +35,8 @@ exports.handler = async () => {
     const { Messages } = await sqs.receiveMessage(receiveParams).promise();
 
     if (!Messages || Messages.length === 0) {
-      console.log("No messages found in SQS queue.");
       return {
-        statusCode: 200,
+        statusCode: 404,
         body: "No messages found in SQS queue.",
       };
     }
@@ -58,30 +53,17 @@ exports.handler = async () => {
     };
 
     await sns.publish(smsParams).promise();
-    // if (AWS_TOPIC_ARN) {
-    //   const snsTopicParams = {
-    //     Message: smsTemplate,
-    //     TopicArn: AWS_TOPIC_ARN,
-    //   };
-
-    //   await sns.publish(snsTopicParams).promise();
-    // }
 
     const deleteParams = {
       QueueUrl: AWS_QUEUE_URL,
       ReceiptHandle: message.ReceiptHandle,
     };
     await sqs.deleteMessage(deleteParams).promise();
-
-   // console.log("Message processed and sent via SMS and optionally published to an SNS topic.");
-
     return {
       statusCode: 200,
       body: "Message processed and sent via SMS and optionally published to an SNS topic.",
     };
   } catch (error) {
-    console.error("Error:", error);
-
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
